37f5dfba612c25db499852c6d00d312f732d096a
Rory Haynes <roryhaynes@mrmacbook.local>
2018-07-25 08:10:31 +0900

    update flag images and currency data model to include all initial currency codes and flags

--- BitCoin Bloodhound/Model/CurrencyModel.swift
+++ BitCoin Bloodhound/Model/CurrencyModel.swift
@@ -16,24 +16,37 @@ class CurrencyModel {
     init(){  //all the data for all the currency
         
 
+        //TODO:- Add currency sign:
         
         
-        
-        listOfCurrencyInformation.append(Currency(code: "AUD", flag: UIImage(named: "GBP")!)) //create and append currency code and flag to the Currency array
-        listOfCurrencyInformation.append(Currency(code: "BRL", flag: UIImage(named: "GBP")!))
-        listOfCurrencyInformation.append(Currency(code: "CAD", flag: UIImage(named: "GBP")!))
-        listOfCurrencyInformation.append(Currency(code: "EUR", flag: UIImage(named: "GBP")!))
+        listOfCurrencyInformation.append(Currency(code: "AUD", flag: UIImage(named: "AUD")!)) //create and append currency code and flag to the Currency array
+        listOfCurrencyInformation.append(Currency(code: "BRL", flag: UIImage(named: "BRL")!))
+        listOfCurrencyInformation.append(Currency(code: "CAD", flag: UIImage(named: "CAD")!))
+        listOfCurrencyInformation.append(Currency(code: "EUR", flag: UIImage(named: "EUR")!))
         listOfCurrencyInformation.append(Currency(code: "GBP", flag: UIImage(named: "GBP")!))
-        
-        //TODO:- Add and update flags and images
-        //TODO:- Add the rest of the members
-        
+        listOfCurrencyInformation.append(Currency(code: "HKD", flag: UIImage(named: "HKD")!))
+        listOfCurrencyInformation.append(Currency(code: "IDR", flag: UIImage(named: "IDR")!))
+        listOfCurrencyInformation.append(Currency(code: "ILS", flag: UIImage(named: "ILS")!))
+        listOfCurrencyInformation.append(Currency(code: "INR", flag: UIImage(named: "INR")!))
+        listOfCurrencyInformation.append(Currency(code: "JPY", flag: UIImage(named: "JPY")!))
+        listOfCurrencyInformation.append(Currency(code: "MXN", flag: UIImage(named: "MXN")!))
+        listOfCurrencyInformation.append(Currency(code: "NOK", flag: UIImage(named: "NOK")!))
+        listOfCurrencyInformation.append(Currency(code: "NZD", flag: UIImage(named: "NZD")!))
+        listOfCurrencyInformation.append(Currency(code: "PLN", flag: UIImage(named: "PLN")!))
+        listOfCurrencyInformation.append(Currency(code: "RON", flag: UIImage(named: "RON")!))
+        listOfCurrencyInformation.append(Currency(code: "RUB", flag: UIImage(named: "RUB")!))
+        listOfCurrencyInformation.append(Currency(code: "SEK", flag: UIImage(named: "SEK")!))
+        listOfCurrencyInformation.append(Currency(code: "SGD", flag: UIImage(named: "SGD")!))
         listOfCurrencyInformation.append(Currency(code: "USD", flag: UIImage(named: "USD")!))
+        listOfCurrencyInformation.append(Currency(code: "ZAR", flag: UIImage(named: "ZAR")!))
+        
+        //TODO:- check API for more supported currencies
+        
+        
         
         
     }
-    //TODO:- add currency and flags to listOfCurrencyInformation Array
-    //let currencyArray = ["HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"] //array of all the currency abbreviations
+    
     
     
     
