22575dc0b33a07816241d1501c6cd9c46106eb64
Rory Haynes <roryhaynes@mrmacbook.local>
2018-07-25 19:33:20 +0900

    implement Alamofire and JSONSwifty, parse JSON data.

--- BitCoin Bloodhound/Controller/ViewController.swift
+++ BitCoin Bloodhound/Controller/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -66,6 +68,48 @@ class ViewController: UIViewController {
         
         finalBitCoinDataUrl = baseBitCoinDataUrl + allCurrency.listOfCurrencyInformation[currencyPickerRow].currencyCode
         print(finalBitCoinDataUrl)
+        
+        getBitCoinData(url: finalBitCoinDataUrl)
+    }
+    
+    func getBitCoinData(url: String) { //method requiring a url that fetches data in the form of a JSON
+        
+        Alamofire.request(url, method: .get).responseJSON { (response) in
+            if response.result.isSuccess{
+                
+                print("bitcoincurrency data received successfully")
+                
+                let bitCoinCurrencyJSON : JSON = JSON(response.result.value!) //force unwrap, as this is only called if a reuslt is found
+                self.updateBitCoinPrice(json: bitCoinCurrencyJSON)
+                
+            }
+            else{
+                
+                print("Error: \(String(describing: response.result.error))")
+            }
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    func updateBitCoinPrice(json : JSON){
+        
+        //TODO:- Implement a menthod to parse the JSon, and update the display.
+        
+        if let bitCoinCurrencyValue = json["ask"].double {
+            
+            print(bitCoinCurrencyValue)
+            
+        }
+        else {
+            
+            print("failed")
+            
+        }
     }
    
 
